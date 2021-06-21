e8c71c834ac7b06519b5931be1dd36c7111b193f
sachin singh <sachinit254@gmail.com>
2021-06-20 19:24:31 -0700

    first commit

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,50 @@
+import React, { useContext, useState } from "react";
+import { GlobalContext } from "../context/GlobalState";
+const AddTransaction = () => {
+  const { addTransaction } = useContext(GlobalContext);
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100),
+      text,
+      amount: +amount,
+    };
+    addTransaction(newTransaction);
+  };
+  return (
+    <>
+      <h3>Add new transaction</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter text..."
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">
+            Amount <br />
+            (negative - expense, positive - income)
+          </label>
+          <input
+            type="text"
+            id="amount"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder="Enter amount..."
+          />
+        </div>
+        <button className="btn" type="submit">
+          Add transaction
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default AddTransaction;
---
+++ src/components/IncomeExpenses.js
@@ -0,0 +1,33 @@
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+const IncomeExpenses = () => {
+  const { transactions } = useContext(GlobalContext);
+  const amounts = transactions.map((transaction) => transaction.amount);
+    const income = amounts
+      .filter((amount) => amount > 0)
+      .reduce((acc, item) => (acc += item))
+      .toFixed(2);
+    const expense = amounts
+      .filter((amount) => amount < 0)
+      .reduce((acc, item) => (acc += item))
+      .toFixed(2);
+  
+  return (
+    <div className="inc-exp-container">
+      <div>
+        <h4>Income</h4>
+        <p id="money-plus" className="money plus">
+          {income}
+        </p>
+      </div>
+      <div>
+        <h4>Expense</h4>
+        <p id="money-minus" className="money minus">
+          {expense}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default IncomeExpenses;
